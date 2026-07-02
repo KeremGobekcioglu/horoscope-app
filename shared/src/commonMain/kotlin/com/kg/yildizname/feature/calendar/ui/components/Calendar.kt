971d4821249c2e862b42d94d6354b8338a092a11
keremgobekcioglu <keko1234keko@outlook.com>
2026-07-03 00:21:52 +0300

    feat: Calendar logic and its composable
    Calendar logic has been ,mplemented. Functions about how to find how many days previous month has , next month has.
    calculateCalendar function implemented, it generates and returns weeks of the month and take account of leading days and trailing days.
    DayComposable is written which is a shining box if day is selected and it s job is represent days on calendar.
    And all of above combined and calendar functionality as a composable is finished but it has some flaws.

--- shared/src/commonMain/kotlin/com/kg/yildizname/feature/calendar/ui/components/Calendar.kt
+++ shared/src/commonMain/kotlin/com/kg/yildizname/feature/calendar/ui/components/Calendar.kt
@@ -1,5 +1,7 @@
 package com.kg.yildizname.feature.calendar.ui.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,15 +11,25 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.kg.yildizname.core.ui.theme.YzBg
 import com.kg.yildizname.core.ui.theme.YzGold
+import com.kg.yildizname.core.ui.theme.YzOnSurface
 import compose.icons.FeatherIcons
 import compose.icons.feathericons.ChevronLeft
 import compose.icons.feathericons.ChevronRight
@@ -25,8 +37,12 @@ import compose.icons.feathericons.ChevronsRight
 import horoscope.shared.generated.resources.Res
 import horoscope.shared.generated.resources.next_month
 import horoscope.shared.generated.resources.previous_month
+import io.ktor.client.request.invoke
 import kotlinx.datetime.LocalDate
+import kotlinx.datetime.Month
 import kotlinx.datetime.isoDayNumber
+import kotlinx.datetime.number
+import kotlinx.datetime.previous
 
 @Composable
 fun Calendar(
@@ -36,6 +52,10 @@ fun Calendar(
     onPreviousMonth: () -> Unit
 )
 {
+    val days : List<String> = listOf(
+        "Pzt","Sa","Ça","Pe","Cu","Cmt","Pzr"
+    )
+    val weeks  = calculateCalendar(date)
     /**
      * < Month Year >
      *
@@ -45,10 +65,11 @@ fun Calendar(
      *  The lucky days bullet is shiny
      */
         Column(
-            modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp)
+            modifier = Modifier.fillMaxSize().padding(horizontal = 32.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
         ) {
 
-            Spacer(Modifier.height(48.dp))
+            Spacer(Modifier.height(32.dp))
             // < Month Year >
             Row(modifier = Modifier.fillMaxWidth(),
                 Arrangement.SpaceBetween,
@@ -78,15 +99,145 @@ fun Calendar(
                 }
             }
 
-            Spacer(Modifier.height(8.dp))
             // Days
-            Row {  }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                days.forEach {
+                        day ->
+                    Text(
+                        text = day,
+                        color = YzGold.copy(0.8f),
+                        modifier = Modifier.weight(1f)
+                    )
+                }
+            }
 
-            Spacer(Modifier.height(8.dp))
-
-
-
+            Column(modifier = Modifier.fillMaxWidth())
+            {
+                weeks.forEach {
+                    week ->
+                    CalendarRow(week = week , selectedDay = 2)
+                }
+            }
     }
+}
+
+data class CalendarWeek(
+    val start: Int,
+    val end: Int
+)
+
+data class CalendarDay(
+    val day: Int,
+    val currentMonth: Boolean
+)
+
+@Composable
+fun DayComposable(modifier: Modifier = Modifier, isLuckyDay: Boolean = false, isSelected: Boolean = false, day: Int)
+{
+    // if selected shine. if luck day , shine. if selected text bold.
+    /**
+     * day number
+     * small dot
+     * rectangle
+     */
+    val shape = RoundedCornerShape(16.dp)
+    Box(
+        modifier = modifier.size(48.dp)
+            .then(
+                if(isSelected || isLuckyDay)
+                {
+                    Modifier.shadow(
+                        elevation = 0.dp,
+                        shape = shape,
+                        ambientColor = YzGold,
+                        spotColor = YzGold
+                    )
+                }
+                else
+                {
+                    Modifier
+                }
+            )
+            .clip(shape)
+//            .background(Color.Transparent)
+            .background(if(isSelected || isLuckyDay) YzBg.copy(0.1f) else Color.Transparent)
+            .then(
+                if(isSelected || isLuckyDay)
+                {
+                    Modifier.border(
+                        width = 2.dp,
+                        color = YzGold,
+                        shape = shape
+                    )
+                }
+                else Modifier
+            ),
+        contentAlignment = Alignment.Center
+    )
+    {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ){
+            Text(
+                text = "$day",
+                color = YzGold,
+                fontSize = 20.sp,
+                fontWeight = if(!isSelected) FontWeight.Normal else FontWeight.Bold
+            )
+            Box(
+                modifier = Modifier.size(4.dp)
+                    .clip(CircleShape)
+                    .background(
+                        if(isSelected) YzGold else YzGold.copy(0.5f))
+            )
+        }
+    }
+}
+
+
+@Composable
+fun CalendarRow(luckDays: List<Int> = mutableListOf() , week: List<CalendarDay> , selectedDay:Int)
+{
+    Row(modifier = Modifier.fillMaxWidth()) {
+        week.forEach {
+            day ->
+                DayComposable(modifier = Modifier.weight(1f), day = day.day , isSelected = day.day == selectedDay)
+        }
+    }
+}
+
+fun calculateCalendar(date: LocalDate) : List<List<CalendarDay>>
+{
+    val firstOfTheMonth = LocalDate(year = date.year,
+        month = date.month,
+        day = 1
+    )
+    val previousMonthDayCount = date.previousMonth().daysInMonth()
+    val daysInMonth = date.daysInMonth()
+    val leading: Int = leadingOffset(firstOfTheMonth)
+    val trailing: Int = trailingOffset(leading,daysInMonth)
+    val calendarDays = mutableListOf<CalendarDay>()
+    val firstPreviousDay = previousMonthDayCount - leading + 1
+
+    // from firstPreviousDay we ll start adding numbers until how many day of its that month - for example if firstPrevDay is 28 , and prev month got 31 day
+    // 28 29 30 31 should be added then we ll complete it to seven.
+    for(day in firstPreviousDay..previousMonthDayCount)
+    {
+        calendarDays.add(CalendarDay(day = day, currentMonth = false))
+    }
+    for(day in 1..daysInMonth)
+    {
+        calendarDays.add(CalendarDay(day = day, currentMonth = true))
+    }
+    for(day in 1..trailing)
+    {
+        calendarDays.add(CalendarDay(day = day, currentMonth = false))
+    }
+    return calendarDays.chunked(7)
 }
 
 fun leadingOffset(firstOfTheMonth: LocalDate) : Int
@@ -103,6 +254,64 @@ fun trailingOffset(leadingOffset: Int, daysInMonth: Int) : Int
     return trailing
 }
 
+fun LocalDate.daysInMonth(): Int = when (month) {
+    Month.JANUARY -> 31
+    Month.FEBRUARY -> if (isLeapYear()) 29 else 28
+    Month.MARCH -> 31
+    Month.APRIL -> 30
+    Month.MAY -> 31
+    Month.JUNE -> 30
+    Month.JULY -> 31
+    Month.AUGUST -> 31
+    Month.SEPTEMBER -> 30
+    Month.OCTOBER -> 31
+    Month.NOVEMBER -> 30
+    Month.DECEMBER -> 31
+}
+fun LocalDate.isLeapYear(): Boolean {
+    val year = this.year
+    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
+}
+
+fun LocalDate.previousMonth() : LocalDate {
+    return (if(month == Month.JANUARY)
+    {
+        LocalDate(
+            year = year - 1,
+            month = Month.DECEMBER,
+            day = 1
+        )
+    }
+    else
+    {
+        val prevMonth = Month.entries[month.ordinal - 1]
+        LocalDate(
+            year = year,
+            month = prevMonth,
+            day = 1
+        )
+    })
+}
+
+fun LocalDate.nextMonth() : LocalDate {
+    return (if(month == Month.DECEMBER)
+    {
+        LocalDate(
+            year = year + 1,
+            month = Month.JANUARY,
+            day = 1
+        )
+    }
+    else
+    {
+        val prevMonth = Month.entries[month.ordinal + 1]
+        LocalDate(
+            year = year,
+            month = prevMonth,
+            day = 1
+        )
+    })
+}
 /**
  *  26 27 28 29 30 1 2
  *  3 4   5   6  7  8  9
