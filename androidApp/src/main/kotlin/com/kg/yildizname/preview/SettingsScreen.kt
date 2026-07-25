8996933c200b4855a7e36b9eeffa5619d1a98509
keremgobekcioglu <k.gobekcioglu2020@gtu.edu.tr>
2026-07-25 16:16:48 +0300

    feat(settings): add preview for SettingsScreen with sample data

---
+++ androidApp/src/main/kotlin/com/kg/yildizname/preview/SettingsScreen.kt
@@ -0,0 +1,35 @@
+package com.kg.yildizname.preview
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.kg.yildizname.core.data.model.ZodiacSign
+import com.kg.yildizname.core.ui.theme.YzBg
+import com.kg.yildizname.core.ui.theme.YzTheme
+import com.kg.yildizname.feature.settings.ui.SettingsScreen
+
+@Preview
+@Composable
+fun SettingsScreenPreview() {
+    YzTheme {
+        Box(modifier = Modifier.fillMaxSize().background(YzBg)) {
+            SettingsScreen(
+                currentSign = ZodiacSign.ARIES,
+                notificationsEnabled = true,
+                notificationTime = "09:00",
+                currentLanguage = "tr",
+                appVersion = "1.0.0",
+                onChangeSignClick = {},
+                onNotificationsEnabledChange = {},
+                onTimeClick = {},
+                onLanguageChange = {},
+                onPrivacyPolicyClick = {},
+                onShareAppClick = {},
+                onResetDataClick = {},
+            )
+        }
+    }
+}
