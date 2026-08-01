bece4b2f5bc68547019e1b9fac84751b4ab6a69b
keremgobekcioglu <k.gobekcioglu2020@gtu.edu.tr>
2026-08-01 14:58:24 +0300

    inset padding changed from 24 dp to 36.dp

--- shared/src/iosMain/kotlin/com/kg/yildizname/core/ui/utils/InsetModifiers.ios.kt
+++ shared/src/iosMain/kotlin/com/kg/yildizname/core/ui/utils/InsetModifiers.ios.kt
@@ -5,5 +5,5 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
-actual fun Modifier.yzStatusBarsPadding(): Modifier = padding(top = 24.dp)
+actual fun Modifier.yzStatusBarsPadding(): Modifier = padding(top = 36.dp)
 actual fun Modifier.yzNavigationBarsPadding(): Modifier = navigationBarsPadding()
