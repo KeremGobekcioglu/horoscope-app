e8170e21eb820eceb234764ec7b1c65aa2c97716
keremgobekcioglu <k.gobekcioglu2020@gtu.edu.tr>
2026-08-03 01:08:45 +0300

    changed this to ios s own status bars padding.

--- shared/src/iosMain/kotlin/com/kg/yildizname/core/ui/utils/InsetModifiers.ios.kt
+++ shared/src/iosMain/kotlin/com/kg/yildizname/core/ui/utils/InsetModifiers.ios.kt
@@ -2,8 +2,9 @@ package com.kg.yildizname.core.ui.utils
 
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
-actual fun Modifier.yzStatusBarsPadding(): Modifier = padding(top = 36.dp)
+actual fun Modifier.yzStatusBarsPadding(): Modifier = statusBarsPadding()
 actual fun Modifier.yzNavigationBarsPadding(): Modifier = navigationBarsPadding()
