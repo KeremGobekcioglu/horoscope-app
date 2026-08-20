d866eac9c2cbefdb45e2f0f9a75d81d5e2613348
keremgobekcioglu <k.gobekcioglu2020@gtu.edu.tr>
2026-08-20 14:14:53 +0300

    version incremented.

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.kg.yildizname"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 6
-        versionName = "1.5"
+        versionCode = 7
+        versionName = "1.6"
     }
     signingConfigs {
         create("release") {
