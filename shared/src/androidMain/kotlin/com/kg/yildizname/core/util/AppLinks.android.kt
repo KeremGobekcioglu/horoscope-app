645893e80762c81f756c85037a641a8243c49683
keremgobekcioglu <k.gobekcioglu2020@gtu.edu.tr>
2026-08-21 14:31:38 +0300

    feat(links): add installUrl for Android and iOS platforms

---
+++ shared/src/androidMain/kotlin/com/kg/yildizname/core/util/AppLinks.android.kt
@@ -0,0 +1,4 @@
+package com.kg.yildizname.core.util
+
+actual val AppLinks.installUrl: String
+    get() = "https://play.google.com/store/apps/details?id=com.kg.yildizname"
---
+++ shared/src/iosMain/kotlin/com/kg/yildizname/core/util/AppLinks.ios.kt
@@ -0,0 +1,4 @@
+package com.kg.yildizname.core.util
+
+actual val AppLinks.installUrl: String
+    get() = AppLinks.LANDING_PAGE
